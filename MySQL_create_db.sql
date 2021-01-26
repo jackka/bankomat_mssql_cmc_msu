60ba3c8799184ec63c88c099bcabdaabf2d6c16b
jackka <jackki@mail.ru>
2021-01-26 17:50:17 +0300

    sync with real base done (db creating and loading - tested)

--- MySQL_create_db.sql
+++ MySQL_create_db.sql
@@ -1,18 +1,20 @@
-drop table if exists atm_state;
-CREATE TABLE `atm_state`(
-	`id` int(11) not NULL,
-	`printed_counter` int(11) NULL,
-	`printer_roll_size` int(11) NULL,
-	`jail_counter` int(11) NULL,
-	`jail_capacity_size` int(11) NULL,
-	`storage_opened` bit NULL,
-	`cur_operation_id` int(11) NULL,
-	`tray_opened` bit NULL,
-	`max_attempts` tinyint NULL
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `atm_state`;
+CREATE TABLE `atm_state` (
+  `id` int(11) NOT NULL,
+  `printed_counter` int(11) DEFAULT NULL,
+  `printer_roll_size` int(11) DEFAULT NULL,
+  `jail_counter` int(11) DEFAULT NULL,
+  `jail_capacity_size` int(11) DEFAULT NULL,
+  `storage_opened` bit(1) DEFAULT NULL,
+  `cur_operation_id` int(11) DEFAULT NULL,
+  `tray_opened` bit(1) DEFAULT NULL,
+  `failed_attempts` tinyint(4) DEFAULT NULL,
+  `max_attempts` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `atm_state`  ADD PRIMARY KEY (`id`);
 
 
+#############сделать view из этой таблицы (из лога попытки, )
 drop table if exists failed_attempts;
 CREATE TABLE `failed_attempts`(
 	`t_stamp` timestamp NULL,
@@ -21,74 +23,79 @@ CREATE TABLE `failed_attempts`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `failed_attempts` ADD PRIMARY KEY (`t_stamp`);
 
+#############сделать view для общей суммы
+разделения суммы из транзакт лога по номиналам
 
-drop table if exists cards;
-CREATE TABLE `cards`(
-	`id` int(11) not NULL,
-	`number` TEXT not NULL,
-	`balance` bigint NULL,
-	`jailed` bit NULL,
-	`blocked` bit NULL,
-	`pin` smallint not NULL
+
+DROP TABLE IF EXISTS `cards`;
+CREATE TABLE `cards` (
+  `id` int(11) NOT NULL,
+  `number` text NOT NULL,
+  `balance` bigint(20) DEFAULT NULL,
+  `jailed` bit(1) DEFAULT NULL,
+  `blocked` bit(1) DEFAULT NULL,
+  `pin` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `cards`  ADD PRIMARY KEY (`id`);
 ALTER TABLE `cards` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 
-drop table if exists cash_storage;
-CREATE TABLE `cash_storage`(
-	`nom_id` tinyint NULL,
-	`nominal` int(11) NULL,
-	`quantity` int(11) NULL
+DROP TABLE IF EXISTS `cash_storage`;
+CREATE TABLE `cash_storage` (
+  `nom_id` tinyint(4) NOT NULL,
+  `nominal` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `cash_storage`  ADD PRIMARY KEY (`nom_id`);
  
  
  
-drop table if exists display_msgs;
-CREATE TABLE `display_msgs`(
-	`id` int(11) NULL,
-	`display_msg` text NULL
+DROP TABLE IF EXISTS `display_msgs`;
+CREATE TABLE `display_msgs` (
+  `id` int(11) NOT NULL,
+  `lang` int(11) NOT NULL,
+  `display_msg` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE `display_msgs` ADD PRIMARY KEY(`id`); 
-ALTER TABLE `display_msgs` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `display_msgs` ADD PRIMARY KEY(`id`,`lang`); 
  
  
  
-drop table if exists jailed_cards;
-CREATE TABLE `jailed_cards`(
-	`log_id` int(11) NULL,
-	`card_id` int(11) NULL,
-	`reason` text NULL
+DROP TABLE IF EXISTS `jailed_cards`;
+CREATE TABLE `jailed_cards` (
+  `log_id` int(11) NOT NULL,
+  `card_id` int(11) NOT NULL,
+  `reason` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `jailed_cards`   ADD PRIMARY KEY (`log_id`,`card_id`);
 
 
-drop table if exists operations;
-CREATE TABLE `operations`(
-	`id` INT(11) NULL,
-	`name` text NULL
+DROP TABLE IF EXISTS `operations`;
+CREATE TABLE `operations` (
+  `id` int(11) NOT NULL,
+  `name` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `operations`   ADD PRIMARY KEY (`ID`);
 ALTER TABLE `operations` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-drop table if exists transacts_log;
-CREATE TABLE `transacts_log`(
-	`id` int(11) NOT NULL,
-	`t_stamp` timestamp NULL,
-	`card_id` int(11) NOT NULL,
-	`op_type` int(11) NULL,
-	`amount` int(11) NULL
+
+
+DROP TABLE IF EXISTS `transacts_log`;
+CREATE TABLE `transacts_log` (
+  `id` int(11) NOT NULL,
+  `t_stamp` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `card_id` int(11) NOT NULL,
+  `op_type` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `transacts_log` ADD PRIMARY KEY (`ID`,`t_stamp`);
 ALTER TABLE `transacts_log` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 
 
-drop table if exists transacts_nomin;
-CREATE TABLE `transacts_nomin`(
-	`transact_id` int NULL,
-	`nom_id` tinyint NULL,
-	`quantity` int(11) NULL
+DROP TABLE IF EXISTS `transacts_nomin`;
+CREATE TABLE `transacts_nomin` (
+  `transact_id` int(11) NOT NULL,
+  `nom_id` tinyint(4) NOT NULL,
+  `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `transacts_nomin`  ADD PRIMARY KEY (`transact_id`,`nom_id`);
